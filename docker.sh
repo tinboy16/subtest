82b419294f3c2ac27ad1f12c9075c6e34164830d
Nguyễn Thương Tín <67304892+tinnguyen162002@users.noreply.github.com>
2022-12-15 16:04:17 +0700

    Add files via upload

---
+++ docker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo apt update && sudo apt upgrade -y
+sudo apt install curl build-essential git wget jq make gcc ack tmux ncdu -y
+sudo wget -qO /usr/local/bin/yq https://github.com/mikefarah/yq/releases/download/v4.27.3/yq_linux_amd64 && chmod +x /usr/local/bin/yq
+apt update && apt install git sudo unzip wget -y
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+curl -SL https://github.com/docker/compose/releases/download/v2.5.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
